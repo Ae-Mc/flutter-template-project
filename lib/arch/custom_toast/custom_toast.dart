17d385fd2b89b7100ee287360864dfc7d7471a15
Ae-Mc <ae_mc@mail.ru>
2022-03-26 17:02:16 +0300

    feat(arch/CustomToast): create CustomToast class for showing error toasts (and others in future)

---
+++ lib/arch/custom_toast/custom_toast.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:flutter_template_project/app/theme/bloc/app_theme.dart';
+
+class CustomToast {
+  final FToast fToast;
+  final BuildContext context;
+
+  CustomToast(this.context) : fToast = FToast()..init(context);
+
+  void showTextFailureToast(String text) {
+    const borderRadius = BorderRadius.all(Radius.circular(25.0));
+    final colorTheme = AppTheme.of(context).colorTheme;
+
+    fToast.showToast(
+      gravity: ToastGravity.TOP,
+      child: InkWell(
+        borderRadius: borderRadius,
+        onTap: () => fToast.removeCustomToast(),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+          decoration: BoxDecoration(
+            borderRadius: borderRadius,
+            color: colorTheme.error,
+          ),
+          child: IntrinsicWidth(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.error_outline_rounded, color: colorTheme.onError),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    text,
+                    style: AppTheme.of(context)
+                        .textTheme
+                        .body1Regular
+                        .copyWith(color: colorTheme.onError),
+                    maxLines: 3,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
