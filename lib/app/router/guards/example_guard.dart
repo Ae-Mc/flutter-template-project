f33f0b8a9f5fe9903f27f1d8b43a59d1a9cdb641
Ae-Mc <ae_mc@mail.ru>
2022-03-26 16:58:27 +0300

    feat(router): add ExampleGuard as guide for creating guards

---
+++ lib/app/router/guards/example_guard.dart
@@ -0,0 +1,18 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_template_project/app/router/app_router.gr.dart';
+
+class ExampleGuard implements AutoRouteGuard {
+  final bool Function() isAuthorized;
+
+  ExampleGuard(this.isAuthorized);
+
+  @override
+  void onNavigation(NavigationResolver resolver, StackRouter router) {
+    if (!isAuthorized()) {
+      // ignore: avoid-ignoring-return-values
+      router.push(const SplashRoute());
+    } else {
+      resolver.next();
+    }
+  }
+}
