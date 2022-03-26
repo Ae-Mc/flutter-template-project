e6b85ec88eab3f227a92d7acda81d449daea0484
Ae-Mc <ae_mc@mail.ru>
2022-03-26 16:58:05 +0300

    refactor(router): remove InitGuard

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter_template_project/app/router/guards/init_guard.dart';
 import 'package:flutter_template_project/features/counter/presentation/pages/counter_page.dart';
 import 'package:flutter_template_project/features/splash/presentation/pages/splash_page.dart';
 
@@ -7,7 +6,7 @@ import 'package:flutter_template_project/features/splash/presentation/pages/spla
   replaceInRouteName: 'Page,Route',
   routes: [
     AutoRoute(page: SplashPage),
-    AutoRoute(page: CounterPage, initial: true, guards: [InitGuard]),
+    AutoRoute(page: CounterPage, initial: true),
   ],
 )
 class $AppRouter {}
